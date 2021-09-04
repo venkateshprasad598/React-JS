b51e70708915fee4d24fdb873dba0fd7eda4d9cd
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-09-04 21:01:24 +0530

    Add

--- bob-course/src/components/Header/Header.js
+++ bob-course/src/components/Header/Header.js
@@ -4,7 +4,11 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <h1 className="header__name">DAILY FRESH</h1>
+      <form className="header__form">
+        <input type="text" className="form-control header__input" />
+        <button className="btn btn-danger header__search">search</button>
+      </form>
     </div>
   );
 };
